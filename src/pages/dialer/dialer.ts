feca75e50f55b322c8ec54bd58cf6ee84d9bb0e2
Rayan Salhab <rayansalhab@hotmail.com>
2018-02-13 15:50:10 +0200

    Test with input type number

--- src/pages/dialer/dialer.ts
+++ src/pages/dialer/dialer.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
 @Component({
@@ -7,17 +7,9 @@ import {NavController} from 'ionic-angular';
 })
 export class DialerPage {
 
-  @ViewChild('tel') tel;
-
   constructor(public navCtrl: NavController) {
 
   }
 
-  ionViewDidEnter() {
-    setTimeout(() => {
-      this.tel.nativeElement.focus();
-      this.tel.nativeElement.scrollIntoView();
-    }, 300);
-  }
 
 }
